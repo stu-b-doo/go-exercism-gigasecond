2118e4687145ef85cf53a07917ddb7d521ddd367
Stuart Bell <stu-b-doo@users.noreply.github.com>
2018-05-29 02:28:02 +0100

    fix doc comments

--- gigasecond.go
+++ gigasecond.go
@@ -1,10 +1,10 @@
-// solution to http://exercism.io/exercises/go/gigasecond/readme
+// Package solution to http://exercism.io/exercises/go/gigasecond/readme.
 package gigasecond
 
 // import path for the time package from the standard library
 import "time"
 
-// AddGigasecond returns a the supplied time with a gigasecond added
+// AddGigasecond returns the supplied time with a gigasecond added. 
 func AddGigasecond(t time.Time) time.Time {
 
 	// Convert the offset to a duration
